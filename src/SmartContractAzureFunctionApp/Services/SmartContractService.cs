2f6cf04824d7c8b599b87ee60cee1d9b3a10181f
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-05-28 09:38:57 +0200

    refactor code

--- src/SmartContractAzureFunctionApp/Services/SmartContractService.cs
+++ src/SmartContractAzureFunctionApp/Services/SmartContractService.cs
@@ -1,6 +1,8 @@
 ﻿using AzureFunctions.Common.Validation;
+using Nethereum.Contracts;
 using Nethereum.Geth;
 using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3.Accounts;
 using SmartContractAzureFunctionApp.Models;
 using System;
@@ -17,20 +19,22 @@ namespace SmartContractAzureFunctionApp.Services
         {
             Guard.NotNull(request, nameof(request));
 
-            var account = new Account(request.CallerPrivateKey);
-
-            var web3 = new Web3Geth(account, request.Endpoint);
+            var web3 = GetWeb3(request);
 
             var contract = web3.Eth.GetContract(request.ContractABI, request.ContractAddress);
 
             var function = contract.GetFunction(request.FunctionName);
 
-            HexBigInteger estimatedGas = await function.EstimateGasAsync(request.FromAddress, null, null, request.FunctionParameters ?? new object[0]);
+            var functionParameters = request.FunctionParameters ?? new object[0];
+
+            HexBigInteger estimatedGas = await function.EstimateGasAsync(request.FromAddress, null, null, functionParameters);
+
+            var result = await function.CallAsync<object>(request.FromAddress, estimatedGas, null, functionParameters);
 
             return new SmartContractFunctionResponse
             {
                 EstimatedGas = (ulong)estimatedGas.Value,
-                Response = await function.CallAsync<object>(request.FromAddress, estimatedGas, null, request.FunctionParameters ?? new object[0])
+                Response = result
             };
         }
 
@@ -38,17 +42,29 @@ namespace SmartContractAzureFunctionApp.Services
         {
             Guard.NotNull(request, nameof(request));
 
-            var account = new Account(request.CallerPrivateKey);
-
-            var web3 = new Web3Geth(account, request.Endpoint);
+            var web3 = GetWeb3(request);
 
             var contract = web3.Eth.GetContract(request.ContractABI, request.ContractAddress);
 
             var function = contract.GetFunction(request.FunctionName);
 
-            HexBigInteger estimatedGas = await function.EstimateGasAsync(request.FromAddress, null, null, request.FunctionParameters ?? new object[0]);
+            var functionParameters = request.FunctionParameters ?? new object[0];
 
-            string transaction = await function.SendTransactionAsync(request.FromAddress, estimatedGas, null, null, request.FunctionParameters ?? new object[0]);
+            HexBigInteger estimatedGas = await function.EstimateGasAsync(request.FromAddress, null, null, functionParameters);
+
+            var receipt = await SendTransactionAsync(request.FromAddress, function, functionParameters, estimatedGas, web3);
+
+            return new SmartContractFunctionResponse
+            {
+                EstimatedGas = (ulong)estimatedGas.Value,
+                Response = receipt != null,
+                TransactionHash = receipt?.TransactionHash
+            };
+        }
+
+        private static async Task<TransactionReceipt> SendTransactionAsync(string fromAddress, Function function, object[] functionParameters, HexBigInteger estimatedGas, Web3Geth web3)
+        {
+            string transaction = await function.SendTransactionAsync(fromAddress, estimatedGas, null, null, functionParameters);
             var receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transaction);
 
             var cts = new CancellationTokenSource(TimeSpan.FromSeconds(TimeoutInSeconds));
@@ -58,12 +74,14 @@ namespace SmartContractAzureFunctionApp.Services
                 receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transaction);
             }
 
-            return new SmartContractFunctionResponse
-            {
-                EstimatedGas = (ulong)estimatedGas.Value,
-                Response = receipt != null,
-                TransactionHash = receipt?.TransactionHash
-            };
+            return receipt;
+        }
+
+        private static Web3Geth GetWeb3(SmartContractFunctionRequest request)
+        {
+            var account = new Account(request.CallerPrivateKey);
+
+            return new Web3Geth(account, request.Endpoint);
         }
     }
 }
